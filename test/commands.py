b211b4221176142a0d2567a70d0261dba289920f
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2007-10-28 08:09:31 +0100

    Removed a verbose=True in the tests

--- test/commands.py
+++ test/commands.py
@@ -777,7 +777,7 @@ class CommandsTestCase(BaseTestCase):
         self.from_xyz("thf01", "pos")
         self.from_cp2k_ener("thf01")
         self.execute("tr-ic-psf", ["tracks/atom.pos", "bond", os.path.join(input_dir, "thf01/init.psf")])
-        self.execute("tr-pca", glob.glob("tracks/atom.pos.bond.*") + ["-n", "-e", "tracks/pca.evals", "-m", "tracks/pca.mode"], verbose=True)
+        self.execute("tr-pca", glob.glob("tracks/atom.pos.bond.*") + ["-n", "-e", "tracks/pca.evals", "-m", "tracks/pca.mode"])
         self.execute("tr-plot", [
             "--xunit=ps", "--yunit=A", "--xlabel=Time", "--ylabel=Amplitude", "--title='First principal bond stretch mode'",
             ":line", "tracks/time", "tracks/pca.mode.0000000",
