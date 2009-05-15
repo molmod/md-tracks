49287476d53774fe0fc4c1f54146aa7a34244b03
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2009-05-15 11:04:57 +0200

    Updated license with mandatory references

--- lib/tracks/util.py
+++ lib/tracks/util.py
@@ -1,6 +1,8 @@
 # MD-Tracks is a statistical analysis toolkit for molecular dynamics
 # and monte carlo simulations.
-# Copyright (C) 2007 - 2008 Toon Verstraelen <Toon.Verstraelen@UGent.be>
+# Copyright (C) 2007 - 2009 Toon Verstraelen <Toon.Verstraelen@UGent.be>, Center
+# for Molecular Modeling (CMM), Ghent University, Ghent, Belgium; all rights
+# reserved unless otherwise stated.
 #
 # This file is part of MD-Tracks.
 #
@@ -8,6 +10,16 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 3
 # of the License, or (at your option) any later version.
+#
+# In addition to the regulations of the GNU General Public License,
+# publications and communications based in parts on this program or on
+# parts of this program are required to cite the following article:
+#
+# "MD-TRACKS: A productive solution for the advanced analysis of Molecular
+# Dynamics and Monte Carlo simulations", Toon Verstraelen, Marc Van Houteghem,
+# Veronique Van Speybroeck and Michel Waroquier, Journal of Chemical Information
+# and Modeling, 48 (12), 2414-2424, 2008
+# DOI:10.1021/ci800233y
 #
 # MD-Tracks is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -59,8 +71,5 @@ class AtomFilter(object):
         if self.filter_atoms is None:
             return True
         return len(self.filter_atoms.intersection(test_indexes)) > 0
-
-
-
 
 
